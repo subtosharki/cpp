80b7bdf550ba0187b7b67f1f161057c29e4d5c0c
subtosharki <camywam@yahoo,com>
2021-08-28 14:42:07 -0400

    all of 3

---
+++ 3 - Conditionals & Logic/Logical Operator and/and.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  
+  int hunger = true;
+  int anger = true;
+  
+  // Write the code below:
+
+  if (hunger == true && anger == true) {
+  
+  std::cout << "Hangry\n";
+
+  }
+  }
+  
+  
