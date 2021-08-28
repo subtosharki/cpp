87490fd5cb199c4be6c7ff869bf6d7b6642bfbf5
subtosharki <camywam@yahoo,com>
2021-08-28 14:14:04 -0400

    some of 2

---
+++ 2 - Variables/Dog Years/dog_years.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  
+  int dog_age = 3;
+  int early_years = 21;
+  int later_years = 4;
+  int human_years = 25;
+
+  std::cout << "My name is dog! Ruff ruff, I am " << human_years << " years old in human years.\n";
+
+
+  
+}
---
+++ 2 - Variables/Tempature Part 2/tempature.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  
+  double tempf;
+  
+  // Ask the user
+  
+  std::cout << "Enter the temperature in Fahrenheit\n";
+  std::cin >> tempf;
+  
+}
